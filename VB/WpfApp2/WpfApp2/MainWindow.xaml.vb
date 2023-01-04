aad2ad894bbe970b6b50b81d81e90455dfda9e2c
Connor Arvin <cjarv986@learner.dvusd.org>
2023-01-04 08:26:34 -0700

    first wpf app

---
+++ VB/WpfApp2/WpfApp2/MainWindow.xaml.vb
@@ -0,0 +1,6 @@
+﻿Class MainWindow
+    Private Const V As String = "hello world"
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        TextBox1.Text = "hello world"
+    End Sub
+End Class
